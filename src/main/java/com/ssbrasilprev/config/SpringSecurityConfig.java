e9a5c812ee71411d57740aaef5a8a5c47c3729f9
ssantos-java <sandro.javac@gmail.com>
2019-11-30 23:27:22 -0200

    limpeza código

--- src/main/java/com/ssbrasilprev/config/SpringSecurityConfig.java
+++ src/main/java/com/ssbrasilprev/config/SpringSecurityConfig.java
@@ -74,15 +74,4 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 ;
     }
 
-    /*@Bean
-    public UserDetailsService userDetailsService() {
-        //ok for teste
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
-    }*/
-
 }
